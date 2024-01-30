920c0cb78916747ebc4cdb93e78a19cc71c217ec
Lawrence-Flash <lawarencetshabalala253@gmail.com>
2024-01-30 11:43:23 -0800

    updates in printf code

--- _printf.c
+++ _printf.c
@@ -1,61 +1,329 @@
 #include "main.h"
-/**
- * _printf - printf function with limited functionality.
- * @format: Format string with directives
- *
- * Description: this function produces output according to a format string
- * It handles the following conversion specifiers: c, s, %.
- * It writes the output to stdout, the standard ouput stream. The function
- * retruns the number of characters printted (excluding the null byte used
- * to end output to strings).
- *
- * Return: The number of characters printed (excluding the null byte).
- */
 
+/**
+ * _printf - Custoome printf function
+ * @format: Format string with format specifiers
+ *
+ * Return: NUmber of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-	int co = 0;i
 	va_list args;
-	int i;
+	int count = 0;
+	char buffer[1024];
+	char *str_arg;
+	int int_arg;
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	while (*format)
 	{
-		if (format[i] != '%')
+		if (*format == '%')
 		{
-			_putchar(format[i]);
-			co++;
+			format++;
+			if (*format == '\0')
+				break;
+
+			count += handle_specifier(*format, args, buffer, &str_arg, &int_arg);
+
 		}
 		else
 		{
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-				{
-					co += _putchar(va_arg(args, int));
-					break;
-				}
-				case 's':
-				{
-					co += _puts(va_arg(args, char *), 4096);
-				}
-				case '%':
-				{
-					co += _putchar('%');
-					break;
-				}
-				default:
-				{
-					co += _putchar('%');
-					co += _putchar(format[i]);
-					break;
-				
-			}
+			buffer[count++] = *format;
+		}
+
+		if (count == 1023)
+		{
+			write(1, buffer, count);
+			count = 0;
+		}
+
+		format++;
+	}
+
+	write(1, buffer, count);
+	va_end(args);
+
+	return count;
+}
+
+/**
+ * handle_non_printable_str - Handle the 'S' specifier in _printf
+ * @args: String argument
+ * @buffer: Local buffer to store the ouput
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_non_printable_str(const char *arg, char *buffer)
+{
+	int count = 0;
+	int i = 0;
+
+	while (arg[i] != '\0')
+	{
+		if (arg[i] < 32 || arg[i] >= 127)
+		{
+			buffer[count++] = '\\';
+			buffer[count++] = 'x';
+			
+			char hex[3];
+			sprintf(hex, "%02X", (unsigned char)arg[i]);
+			count += handle_str(hex, buffer);
+		}
+		else
+		{
+			buffer[count++] = arg[i];
+		}
+		if (count == 1023)
+		{
+			write(1, buffer, count);
+			count = 0;
+		}
+		
+		
+		i++;
+	}
+	return count;
+}
+
+/**
+ * handle_specifier - handle format specifier in _printf
+ * @specifier: format specifier character
+ * @args: variable arguments list
+ * @buffer: local buffer to store the output
+ * @str-arg: pointer to a string argument (for S Specifier)
+ * @int_arg: pointer to an integer argument (for some specifiers)
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_specifier(char specifier, va_list args, char *buffer, char **str_arg, int *int_arg)
+{
+	int count = 0;
+
+	switch (specifier)
+	{
+		case 'c':
+			count += handle_char(va-arg(args, int), buffer);
+			break;
+		case 's':
+			*str_arg = va_arg(args, char *);
+			count += handle_str(*str_arg, buffer);
+			break;
+		case '%':
+			count += handle_percent(buffer);
+			break;
+		case 'd':
+		case 'i':
+			*int_arg = va_arg(args, int);
+			count += handle_int(*int_arg, buffer);
+			break;
+		case 'b':
+			*int_arg = va_arg(args, unsigned int);
+			count += handle_binary(*int_arg, buffer);
+			break;
+		case 'S':
+			*str_arg = va_arg(args, unsigned int);
+			count += handle_non_printable_str(*str_arg, buffer);
+			break;
+		default:
+			break;
+	}
+
+	return count;
+}
+
+/**
+ * handle_char - Handle the 'c' specifier in _printf
+ * @arg: Character argument
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_char(int arg, char *buffer)
+{
+	buffer[0] = arg;
+	return 1;
+}
+
+/**
+ * handle_str - Handle the 's' specifier in _printf
+ * @arg: String argument
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_str(const char *arg, char *buffer)
+{
+	int count = 0;
+	int i = 0;
+	
+	while (arg[i] != '\0')
+	{
+		buffer[count++] = arg[i++];
+		if (count == 1023)
+		{
+			write(1, buffer, count);
+			count = 0;
 		}
 	}
 
-	va_end(args);
-	return co;
+	return count;
 }
+
+/**
+ * handle_percent - Handle the '%' specifier in _printf
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_percent(char *buffer)
+{
+	buffer[0] = '%';
+	return 1;
+}
+
+/**
+ * handle_int - Handle the 'd' and 'i' specifier in _printf
+ * @arg: Integer argument
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed for the spcifier
+ */
+int handle_int(int arg, char *buffer)
+{
+	int count = sprintf(buffer, "%d", arg);
+	return count;
+}
+
+/**
+ * handle_custom_specifier - Handle custom specifier in _printf
+ * @specifier: Custom specifier character
+ * @args: Variable arguments list
+ * @buffer: local buffer to store the output
+ * @str_arg: Pointer to a string argument (for S specifier)
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_custom_specifier(char specifier, va_list args, char *buffer, char **str_arg)
+{
+	int count = 0;
+
+	switch (specifier)
+	{
+		case 'b':
+			count += handle_binary(va_arg(args, unsigned int), buffer);
+			break;
+		case 'S':
+			*str-arg = va_arg(args, char *);
+			count += handle_non_printable_str(*str_arg, buffer);
+			break;
+		case 'r':
+			*str_arg = va_arg(args, char *);
+			count += handle_reversed_str(*str_arg, buffer);
+			break;
+		case 'R':
+			*str_arg = va_arg(args, char *);
+			count += handle_rot13_str(*str_arg, buffer);
+			break;
+		default:
+			break;
+	}
+
+	return count;
+}
+
+/**
+ * handle_binary - handle the 'b' specifier in the _printf
+ * @arg: Unsigned integer argument
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed for the specifier
+ */
+int handle_binary(unsigned int arg, char *buffer)
+{
+	if (arg == 0)
+	{
+		buffer[0] = '0';
+		return 1;
+	}
+
+	int num_bits = 0;
+	unsigned int temp = arg;
+	while (temp > 0)
+	{
+		temp >>= 1;
+		num_bits++;
+	}
+	int i;
+	for (i = num_bits - 1; i >= 0; i--)
+	{
+		buffer[num_bits - 1 - i] = ((arg >> i) & 1) ? '1' : '0';
+	}
+
+	return num_bits;
+}
+
+/**
+ * handle_reversed_str - Handle the 'r' specifier in _printf
+ * @arg: String argument
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed to the specifier
+ */
+int handle_reversed_str(const char *arg, char *buffer)
+{
+	if (arg == NULL)
+		return 0;
+
+	int length = 0;
+	while (arg[length] != '\0')
+	{
+		length++;
+	}
+	
+	int i, j;
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		char temp = arg[i];
+		buffer[i] = arg[j];
+		buffer[j] = temp;
+	}
+
+	return length;
+}
+
+/**
+ * handle_rot13_str - Handle the 'R' specifier in _printf
+ * @arg: String argument
+ * @buffer: Local buffer to store the output
+ *
+ * Return: Number of characters printed for the specifier
+ */
+
+int handle_rot13_str(const char *arg, char *buffer)
+{
+	if (arg == NULL)
+		return 0;
+	int count = 0;
+	char current_char;
+	char base;
+	while(arg[count] != '\0')
+	{
+		current_char = arg[count];
+
+		if ((current_char >= 'A' && current_char <= 'Z') || (current_char >= 'a' && current_char <= 'z'))
+		{
+			base = (current_char >= 'A' && current_char <= 'Z') ? 'A' : 'a';
+			buffer[count] = (current_char - base + 13) % 26 + base;
+		}
+		else
+		{
+			buffer[count] = current_char;
+		}
+
+		count++;
+	}
+
+	return count;
+}
+
--- main.h
+++ main.h
@@ -4,15 +4,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 
-//helper functions for handling format specifiers
-void print_char(char c, int *count);
-void print_string(const char *str, int *count);
-void print_integer(int n, int *count);
-void print_unsigned(unsigned int n, int *count);
-
-
-#endif
+#endif 
